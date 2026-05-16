8ce3358e936fdee602ae4ba587f74cebd336ed58
yusukeyamanaka198510-max <yusukeyamanaka198510@gmail.com>
2026-05-16 22:22:53 +0900

    hero: remove mix-blend-mode for transparent png owl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -164,7 +164,7 @@ export default function HomePage() {
                   width={420}
                   height={420}
                   className="relative w-[220px] sm:w-[300px] lg:w-[400px] xl:w-[440px] object-contain"
-                  style={{ mixBlendMode: "lighten" }}
+
                   priority
                 />
               </div>
